721ab47413e26ace2c06795c18d9a2fbf84e1f98
Addynene <chikukwaadleen@example.com>
2024-07-24 23:54:53 -0400

    This is a commit statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css',
+   /* 
+    '../../src/assets/css/bootstrap.min.css',
+    '../../src/assets/css/animate.min.css',
+    '../../src/assets/css/icofont.min.css',
+    '../../src/assets/css/magnific-popup.min.css',
+    '../../src/assets/css/meanmenu.css',
+    '../../src/assets/css/odometer.min.css',
+    '../../src/assets/css/owl.carousel.min.css',
+    '../../src/assets/css/owl.theme.default.min.css',
+    '../../src/assets/css/responsive.css',
+    '../../src/assets/css/slick-theme.min.css',
+    '../../src/assets/css/slick.min.css',
+    '../../src/assets/css/theme-dark.css',*/
+  ]
 })
 export class AppComponent {
   title = 'MoHCC-Latest';
